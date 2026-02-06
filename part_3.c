cd77c0c4036072068210fcfee7279d2a77bb981c
Aeternis12 <lmm1409@yahoo.com>
2026-02-06 09:43:36 -0500

    Add files via upload
    adding part 3

---
+++ part_3.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <string.h> 
+#include <stdio.h>
+
+char* expand_tilde(char* token){
+	if(token[0] != '~'){
+		return token;
+	}
+
+	char* home = getenv("HOME");
+	if(home == NULL) {
+		return token;
+	}
+
+	if(token[1] == '\0'){
+		return strdup(home);
+	}
+
+	if(token[1] == '/'){
+		char* expanded = malloc(strlen(home) + strlen(token));
+
+		strcpy(expanded, home);
+		strcat(expanded, token + 1);
+
+		return expanded;
+	}
+
+return token;
+}
+
+int main() {
+    // List of test cases
+    char *test_cases[] = {"~", "~/", "~/Documents", "file~name", "not_a_tilde", NULL};
+    
+    printf("Testing Tilde Expansion:\n");
+    for (int i = 0; test_cases[i] != NULL; i++) {
+        char *input = test_cases[i];
+        char *output = expand_tilde(input);
+        
+        printf("Input:  %s\n", input);
+        printf("Output: %s\n\n", output);
+        
+        // Clean up memory if expansion happened
+        if (output != input) free(output);
+    }
+    return 0;
+}
