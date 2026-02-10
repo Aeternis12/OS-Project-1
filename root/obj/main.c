c0e9afd6b93beefa7a17e1c288fd2af177467dc5
maxh142 <83785442+maxh142@users.noreply.github.com>
2026-02-09 20:45:26 -0500

    Update main.c

--- root/obj/main.c
+++ root/obj/main.c
@@ -1,1 +1,17 @@
-//main file
+#include "shell.h"
+#include "lexer.h"
+
+int main(void) {
+    print_prompt();
+
+    char *input = get_input();
+    if (!input)
+        break;
+
+    tokenlist *tokens = get_tokens(input);
+    if (!tokens || tokens->size == 0) {
+        free(input);
+        free_tokens(tokens);
+        continue;
+    }
+}
